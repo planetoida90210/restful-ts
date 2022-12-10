f01a857ce94efffd653334b2c70184f63470fa7a
planetoida90210 <galeriakoloru90210@gmail.com>
2022-12-10 23:50:57 +0100

    create api rules

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import exrpess from 'express';
+import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 
 import { config } from './config/config';
 import Logging from './library/Logging';
 
-const router = exrpess();
+const router = express();
 
 // connect to mongo
 
@@ -15,8 +15,59 @@ mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
         Logging.info('Connected to mongodb');
+        StartServer();
     })
     .catch((error) => {
         Logging.error('Unable to connect:');
         Logging.error(error);
     });
+
+// only start the server if Mongo connects
+const StartServer = () => {
+    /** Log the request */
+    router.use((req, res, next) => {
+        /** Log the req */
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /** Log the res */
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Rules of API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('Not found');
+
+        Logging.error(error);
+
+        res.status(404).json({
+            message: error.message
+        });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
